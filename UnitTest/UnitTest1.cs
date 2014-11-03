1d8d7f3fb6e9431926ff63920e9ea733e9fcd60a
ccljason <jason.c.leung@gmail.com>
2014-11-03 00:58:48 -0800

    Added unit test

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+   [TestClass]
+   public class UnitTest1
+   {
+      [TestMethod]
+      public void TestMethod1()
+      {
+         // theory:
+         //http://stackoverflow.com/questions/1980108/verifying-a-method-was-called
+
+         // Try this:
+         //http://stackoverflow.com/questions/20863180/in-a-unit-test-do-you-verify-and-assert
+
+      }
+   }
+}
